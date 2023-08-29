e4d76cbe1ad3c4052a4b770eead5d9c41ba9cf25
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-08-29 22:19:32 +0300

    Add project files.

---
+++ PracticeAndReview/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class icerisindeki degerlere klavyeden veri girisi
+
+            Musteri m = new Musteri();
+            Console.WriteLine("Id degerini girin : ");
+            m.id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adiniz : ");
+            m.ad = Console.ReadLine();
+            Console.WriteLine("Soyadiniz : ");
+            m.soyad = Console.ReadLine();
+            Console.WriteLine("Sehriniz : ");
+            m.sehir = Console.ReadLine();
+
+            Console.WriteLine("*** Musteri Kimlik Karti ***");
+            Console.WriteLine();
+            Console.WriteLine("Id : " + m.id);
+            Console.WriteLine("Ad : " + m.ad);
+            Console.WriteLine("Soyad  :" + m.soyad);
+            Console.WriteLine("Sehir : " + m.sehir);
+
+            Console.Read();
+        }
+    }
+}
